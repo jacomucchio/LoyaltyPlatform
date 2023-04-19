d3a8d15ef886d0f845439c194dd7bffec84dc2ea
Jacopo Mucchietto <jacopo.mucchietto@studenti.unicam.it>
2023-04-19 16:09:58 +0200

    Initial commit

---
+++ src/test/java/it/unicam/cs/ids/loyaltyplatform/loyaltyplatform/LoyaltyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.loyaltyplatform.loyaltyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
