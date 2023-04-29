04e334e066439df7a4a1168ea1a88db032a630b1
Lorenzo Paolucci <lorenzo.paolucci@studenti.unicam.it>
2023-04-29 16:49:07 +0200

    Modified Company class.

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/model/Company.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/model/Company.java
@@ -1,4 +1,26 @@
 package it.unicam.cs.ids.loyaltyplatform.model;
 
 public class Company {
+    private String name;
+    private String emailAddress;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
 }
+
+
