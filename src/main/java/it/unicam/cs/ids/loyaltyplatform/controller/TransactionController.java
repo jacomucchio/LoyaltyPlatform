1864092252e3f70ea0037a6f4e649c773796489b
Samuele Mucchietto <samuele.mucchietto@studenti.unicam.it>
2023-05-03 11:02:00 +0200

    Implementation of Transaction Service and Transaction Controller

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/TransactionController.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/controller/TransactionController.java
@@ -1,4 +1,34 @@
 package it.unicam.cs.ids.loyaltyplatform.controller;
+import it.unicam.cs.ids.loyaltyplatform.persistence.entity.TransactionEntity;
+import it.unicam.cs.ids.loyaltyplatform.service.TransactionService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@RestController
+@RequestMapping("/Transaction")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @GetMapping("/")
+    public List<TransactionEntity> findAll(){
+        return transactionService.findAll();
+    }
+    @GetMapping("/{id}")
+    public TransactionEntity findById(@PathVariable Integer id){
+        return transactionService.findById(id);
+    }
+
+    @PostMapping("/")
+    public void save(TransactionEntity transactionEntity)
+    {
+        transactionService.save(transactionEntity);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id)
+    {
+        transactionService.deleteById(id);
+    }
 }
--- src/main/java/it/unicam/cs/ids/loyaltyplatform/service/TransactionService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/service/TransactionService.java
@@ -1,4 +1,31 @@
 package it.unicam.cs.ids.loyaltyplatform.service;
 
+import it.unicam.cs.ids.loyaltyplatform.persistence.entity.TransactionEntity;
+import it.unicam.cs.ids.loyaltyplatform.persistence.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
 public class TransactionService {
-}
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+    public List<TransactionEntity> findAll(){return transactionRepository.findAll();}
+
+    public TransactionEntity findById(Integer id){
+        return transactionRepository.findById(id)
+                .orElseThrow(()-> new NoSuchElementException("Transaction not found with id: "+id));
+    }
+
+    public void save(TransactionEntity transactionEntity){transactionRepository.save(transactionEntity);}
+    public void deleteById(Integer id){transactionRepository.deleteById(id);}
+
+    }
+
+
