2e4184bfe023076c1b62b2016cf67356c4339b6a
Lorenzo Paolucci <lorenzo.paolucci@studenti.unicam.it>
2023-05-17 15:41:31 +0200

    test branch.

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/transaction/TransactionService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/transaction/TransactionService.java
@@ -33,7 +33,7 @@ public class TransactionService {
 
     /*
     TODO: -applyBenefits se dovrebbe modificare l'importo della transazione in modoc che quando
-           viene riutilizzato applyBenefits viene applicato con la transazione aggiornata mark pici
+           viene riutilizzato applyBenefits viene applicato con la transazione aggiornata trek
      */
 
     public TransactionEntity validateTransaction(CompanyEntity company, CardEntity card, double amount) {
