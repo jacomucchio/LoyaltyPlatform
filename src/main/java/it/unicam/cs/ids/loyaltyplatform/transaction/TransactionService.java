49a362bc74a8232f6b858fa7a4290039ff135cff
Lorenzo Paolucci <lorenzo.paolucci@studenti.unicam.it>
2023-05-17 15:35:53 +0200

    Fixed CompanyService methods delete and addNewCompany.

--- src/main/java/it/unicam/cs/ids/loyaltyplatform/transaction/TransactionService.java
+++ src/main/java/it/unicam/cs/ids/loyaltyplatform/transaction/TransactionService.java
@@ -33,7 +33,7 @@ public class TransactionService {
 
     /*
     TODO: -applyBenefits se dovrebbe modificare l'importo della transazione in modoc che quando
-           viene riutilizzato applyBenefits viene applicato con la transazione aggiornata klea pici
+           viene riutilizzato applyBenefits viene applicato con la transazione aggiornata mark pici
      */
 
     public TransactionEntity validateTransaction(CompanyEntity company, CardEntity card, double amount) {
